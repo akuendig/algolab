d779e4a7718d1406dd6b65c891427458d3d33e68
Pascal Spörri <pascal.spoerri@gmail.com>
2013-01-04 18:18:11 +0100

    beasts

--- week 3 - fundamental2/01/beasts/main.cpp
+++ week 3 - fundamental2/01/beasts/main.cpp
@@ -1,131 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
+#include <algorithm>
 
 using namespace std;
 
 int prev_run;
 int counter_fallof;
 
-
-int compute_conflicts(vector<int>& grid, int n) {
-    int conflicts = 0;
-    for (int i=1; i<=n; i++) {
-        int c = grid[i];
-        for (int j=1; j< n; j++) {
-            if ((i-j) > 0) {
-                int t = grid[i-j];
-                if (t == c-j || t == c+j) {
-                    conflicts++;
-//                    break;
-                }
-            }
-
-            if ((i+j) <= n) {
-                int t = grid[i+j];
-                if (t == c-j || t == c+j) {
-                    conflicts++;
-//                    break;
-                }
-            }
-
-        }
-    }
-//    cout << conflicts << endl;
-    return conflicts;
-}
-
-bool swapper(vector<int>& grid, int n) {
-    if (n == 1)
-        return true;
-    int conflicts1 = compute_conflicts(grid, n);
-    int conflicts2 = conflicts1;
-    for (int i=1; i <= n; i++) {
-//        cout << i << endl;
-        for (int j=i+1; j <= n; j++) {
-            int tmp = grid[i];
-            grid[i] = grid[j];
-            grid[j] = tmp;
-
-            conflicts2 = compute_conflicts(grid, n);
-
-            if (conflicts2 == 0) {
-                return true;
-            }
-            if (conflicts2 < conflicts1) {
-                conflicts1 = conflicts2;
-//                i = 1;
-
-//                continue;
-                break;
-            }
-            tmp = grid[i];
-            grid[i] = grid[j];
-            grid[j] = tmp;
-        }
-//        cout << i << endl;
-
-    }
-    return false;
-}
-
-
+#define MAX_GRID 100
+bool tried[MAX_GRID+1][MAX_GRID+1];
+int grid[MAX_GRID+1];
+int n;
 // Recursive Method: Slow
-bool recursion(vector<int>& grid, int depth, int n) {
-//    cout << depth << endl;
+bool recursion(int depth) {
     if (depth > n) {
         return true;
     }
-    // Reuse the values
-    if (prev_run > n && depth <= prev_run) {
-        if (recursion(grid, depth+1, n)) {
-            return true;
-        }
-        prev_run = -1;
-    }
-    if (depth == 1) {
-        for (int i=1; i<= n; i++) {
-            grid[depth] = i;
-            if (recursion(grid, depth+1, n)) {
-                return true;
+
+    int irun;
+    tried[depth][0] = true;
+    fill(&tried[depth][1], &tried[depth][n+1],false);
+    int z = n+1;
+    for (int i=0; i<n; i++) {
+        while(true) {
+            irun = (rand())%(z);
+            if (!tried[depth][irun]) {
+                break;
             }
+//            cout << depth << " " << irun << endl;
         }
-        return false;
-    }
-    int counter = 0;
-    for (int i=1; i<= n; i++) {
-//        grid[depth] = i;
-//        if (grid[depth-1] == (i-1) || grid[depth-1] == (i+1)) {
-//            // i++;
-//            continue;
-//        }
+        tried[depth][irun] = true;
+
         bool cont = false;
         for (int j=1; j < depth; j++) {
             int l = grid[depth-j];
-            if (grid[j] == i || l == (i-j) ||l == (i+j)) {
+            if (grid[j] == irun || l == (irun-j) ||l == (irun+j)) {
                 cont = true;
                 break;
             }
         }
         if (cont) {
-            if (n > 40) {
-                if (counter > counter_fallof) {
-                    return false;
-                }
-                counter++;
-                i = rand()%(n+1);
-            } else if (n > 20) {
-                if (counter > 30) {
-                    return false;
-                }
-                counter++;
-                i = rand()%(n+1);
-            }
             continue;
         }
-        grid[depth] = i;
 
-        if (recursion(grid, depth+1, n)) {
+        grid[depth] = irun;
+
+        if (recursion(depth+1)) {
             return true;
         }
     }
@@ -133,15 +54,13 @@ bool recursion(vector<int>& grid, int depth, int n) {
 }
 
 void testcase() {
-    int n;
+//    int n;
     cin >> n;
-    vector<int> grid(n+1,0);
-    for (int i=0; i < n+1; i++) {
-        grid[i] = i;
-    }
-    counter_fallof = int(double(n)*0.95);
-
-    if (swapper(grid, n)) {
+//    vector<int> grid(n+1,0);
+//    for (int i=0; i < n+1; i++) {
+//        grid[i] = i;
+//    }
+    if (recursion(1)) {
         for (int i=1; i<=n; i++) {
             cout << grid[i] << " ";
         }
@@ -154,6 +73,7 @@ void testcase() {
 
 int main()
 {
+    srand(time(NULL));
     cin.sync_with_stdio(false);
     cout.sync_with_stdio(false);
     int testcases;
