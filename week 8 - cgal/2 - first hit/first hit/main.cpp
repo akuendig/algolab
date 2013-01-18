15ceec737a63cb89021d1841db7afe4a8208259b
Pascal Spörri <pascal@spoerri.io>
2013-01-18 16:37:11 +0100

    First Hit

--- week 8 - cgal/2 - first hit/first hit/main.cpp
+++ week 8 - cgal/2 - first hit/first hit/main.cpp
@@ -1,14 +1,13 @@
-#include <CGAL/Exact_predicates_exact_constructions_kernel_with_sqrt.h>
-#include <CGAL/Min_circle_2.h>
-#include <CGAL/Min_circle_2_traits_2.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+
+
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <algorithm>
 
-typedef CGAL::Exact_predicates_exact_constructions_kernel_with_sqrt K;
-typedef CGAL::Min_circle_2_traits_2<K>  Min_Circle_Traits;
-typedef  CGAL::Min_circle_2<Min_Circle_Traits> Min_Circle;
+typedef CGAL::Exact_predicates_exact_constructions_kernel K;
 using namespace std;
+
 double floor_to_double(const K::FT& x)
 {
   double a = std::floor(CGAL::to_double(x));
@@ -24,32 +23,71 @@ double ceil_to_double(const K::FT& x)
   return a;
 }
 
+void shorten_segment(K::Segment_2 &s, CGAL::Object o) {
+    if (const K::Point_2 *p = CGAL::object_cast<K::Point_2>(&o)) {
+        s = K::Segment_2(s.source(), *p);
+    } else if (const K::Segment_2 *t = CGAL::object_cast<K::Segment_2>(&o)) {
+        // select endpoint of *t closer to s.source()
+        if (CGAL::collinear_are_ordered_along_line(s.source(), t->source(), t->target())) {
+            s = K::Segment_2(s.source(), t->source());
+        } else {
+          s = K::Segment_2(s.source(), t->target());
+        }
+    } else {
+        throw runtime_error("Strange Error");
+    }
+}
+
 void testcases() {
-    vector<K::Point_2> points;
-    cout << fixed << setprecision(0);
+//    cout << fixed << setprecision(0);
+    std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
+
     while(true) {
-        int n;
+        size_t n;
         cin >> n;
 
-        if (n <= 0)
+        if (n == 0)
             break;
 
-        points.reserve(n);
-        for (int i=0; i <n; i++) {
-//            K::Point_2 p;
-//            cin >> p;
-            double x, y;
-            cin >>x >> y;
-//            p(x,y);
-            K::Point_2 p(x, y);
-            points.push_back(p);
-        }
+        double ix, iy, jx, jy;
+//        cin >> ix >> iy >> jx >> jy;
+//        K::Ray_2 r(K::Point_2(ix, iy), K::Point_2(jx, jy));
+        K::Ray_2 r;
+        cin >> r;
+        K::Segment_2 rc(r.source(), r.source());
 
-        Min_Circle mc(points.begin(), points.begin()+n, true);
-        Min_Circle_Traits::Circle c = mc.circle();
-        K::FT r = sqrt(c.squared_radius()); // you need Exact_predicates_exact_constructions_kernel_with_sqrt
-        cout << ceil_to_double(r) << endl;
-        points.clear();
+        vector<K::Segment_2> segments;
+        segments.reserve(n);
+
+        size_t i = 0;
+        for (; i<n; ++i) {
+            // We are losing accuracy
+            cin >> ix >> iy >> jx >> jy;
+            segments.push_back(K::Segment_2(K::Point_2(ix, iy), K::Point_2(jx, jy)));
+//            K::Segment_2 ss;
+//            cin >> ss;
+//            segments.push_back(ss);
+        }
+//        random_shuffle(segments.begin(), segments.end());
+
+        for (i=0; i<n; ++i) {
+            if (CGAL::do_intersect(segments[i], r)) {
+                shorten_segment(rc, CGAL::intersection(r, segments[i]));
+                break;
+            }
+        }
+        if (i >= n) {
+            cout << "no" << endl;
+            continue;
+        }
+        while (++i < n) {
+            if (CGAL::do_intersect(segments[i], rc)) {
+                shorten_segment(rc, CGAL::intersection(r, segments[i]));
+            }
+        }
+        cout << floor_to_double(rc.target().x()) << " "
+                << floor_to_double(rc.target().y()) << endl;
+        segments.clear();
     }
 }
 
