14b40ca079a4c63240c5621ab1758ad3e3a88624
Pascal Spörri <pascal@spoerri.io>
2013-01-19 13:11:37 +0100

    Graypes

--- week 9 - proximity structures/0 - graypes/graypes/main.cpp
+++ week 9 - proximity structures/0 - graypes/graypes/main.cpp
@@ -1,8 +1,14 @@
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+#include <CGAL/squared_distance_2.h>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
-typedef CGAL::Exact_predicates_exact_constructions_kernel K;
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Delaunay_triangulation_2<K>  Triangulation;
+typedef Triangulation::Finite_edges_iterator  Edge_iterator;
+
 using namespace std;
 
 double floor_to_double(const K::FT& x)
@@ -14,35 +20,42 @@ double floor_to_double(const K::FT& x)
 }
 
 void testcases() {
-    while(true) {
-        int n;
-        cin >> n;
-        if (n == 0) {
-            break;
+    size_t n;
+    std::vector<Triangulation::Point> points;
+    while (cin >> n && n > 0) {
+        points.reserve(n);
+        for (size_t i = 0; i < n; i++) {
+            Triangulation::Point p;
+            cin >> p;
+            points.push_back(p);
         }
-        double lx, ly, rx, ry;
-        cin >> lx >> ly >> rx >> ry;
-        K::Ray_2 r(K::Point_2(lx, ly), K::Point_2(rx, ry));
-        bool intersected = false;
-        for (int i=0; i < n; i++) {
-            cin >> lx >> ly >> rx >> ry;
-            if (!intersected) {
-                K::Segment_2 s(K::Point_2(lx, ly), K::Point_2(rx, ry));
-                if (CGAL::do_intersect(r,s)) {
-                    intersected = true;
-                }
+
+        Triangulation t;
+        t.insert(points.begin(), points.end());
+
+        double distance = -1;
+        for (Edge_iterator it = t.finite_edges_begin(); it != t.finite_edges_end(); it++) {
+//            K::Ft d = CGAL::squared_distance()
+//            K::FT d = CGAL::squ
+            Triangulation::Segment seg = t.segment( it );
+
+            Triangulation::Point p0 = seg.point(0);
+            Triangulation::Point p1 = seg.point(1);
+            double d = CGAL::to_double(CGAL::squared_distance(p0, p1));
+//            cout << d << endl;
+            if (d < distance || distance < 0.0) {
+                distance = d;
             }
         }
-        if (intersected) {
-            cout << "yes" << endl;
-        } else {
-            cout << "no" << endl;
-        }
+
+        cout << ceil(sqrt(distance)/2.0*100.0) << endl;
+        points.clear();
     }
 }
 
 int main()
 {
+    std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
     cin.sync_with_stdio(false);
     cout.sync_with_stdio(false);
     testcases();
