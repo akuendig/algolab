a58717c1522c13e851f2aa662fe2e637f4eacf99
Pascal Spörri <pascal.spoerri@gmail.com>
2013-01-21 15:10:48 +0100

    Diet

--- week 10 - linear and quadratic programming/1 - diet/diet/main.cpp
+++ week 10 - linear and quadratic programming/1 - diet/diet/main.cpp
@@ -30,75 +30,60 @@ int ceil_to_double(const CGAL::Quotient<ET>& x) {
 
 void testcases() {
     size_t p;
-	const int X = 0;
-	const int Y = 1;
-	const int ZZ = 2;
-	int a, b;
+//	const int X = 0;
+//	const int Y = 1;
+//	const int ZZ = 2;
 
-    while (cin >> p && p > 0) {
-    	int a, b;
-    	cin >> a >> b;
+	int num_nutrients, num_foods;
+	int nutrients[41][2];
+	int food_nutrients[101][41];
+	int	prices[101];
+    while (true) {
+    	cin >> num_nutrients >> num_foods;
+    	if (num_nutrients == 0 && num_foods == 0) {
+    		break;
+    	}
+    	for (int i=0; i<num_nutrients; i++) {
+    		cin >> nutrients[i][0] >> nutrients[i][1];
+    	}
+    	for (int i=0; i<num_foods; i++) {
+    		cin >> prices[i];
+    		for (int j=0; j<num_nutrients; j++) {
+    			cin >> food_nutrients[i][j];
+    		}
+    	}
+    	int FOODS = 0;
+    	Program qp (CGAL::SMALLER, true, 0, false, 0);
+    	// function to minimize
+    	for (int i=0; i < num_foods; i++) {
+    		qp.set_c(FOODS+i, prices[i]);
+    	}
 
-        Program qp1 (CGAL::SMALLER, true, 0, false, 0);
-        // -b*y + a*x^2
-        	qp1.set_c(Y, -b);
-        	qp1.set_d(X, X, 2*a);
+    	int eq_counter = 0;
 
-        // x + y <= 4
-    		qp1.set_a(X, 0, 1);
-    		qp1.set_a(Y, 0, 1);
-    		qp1.set_b(0, 4);
-    	// 4x + 2y <= ab
-    		qp1.set_a(X, 1, 4);
-    		qp1.set_a(Y, 1, 2);
-    		qp1.set_b(1, a*b);
+    	for (int i=0; i<num_nutrients; i++) {
+    		for (int j=0;j<num_foods;j++) {
+        		qp.set_a(FOODS+j,eq_counter,food_nutrients[j][i]);
+    		}
+    		qp.set_b(eq_counter, nutrients[i][1]);
+    		eq_counter++;
+    	}
 
-    	// -x + y <= 1
-    		qp1.set_a(X, 2, -1);
-    		qp1.set_a(Y, 2, 1);
-    		qp1.set_b(2, 1);
+    	for (int i=0; i<num_nutrients; i++) {
+    		for (int j=0;j<num_foods;j++) {
+        		qp.set_a(FOODS+j,eq_counter,food_nutrients[j][i]);
+    		}
+    		qp.set_b(eq_counter, nutrients[i][0]);
+    		qp.set_r(eq_counter, CGAL::LARGER);
+    		eq_counter++;
+    	}
 
-    	// X >= 0
-//    		qp1.set_l(X, true, 0);
-    	// Y >= 0
-//   		qp1.set_l(Y, true, 0);
-
-    	Program qp2 (CGAL::LARGER, false, 0, false, 0);
-    	// a*x^2 + b*y + zz^2
-    		qp2.set_d(X, X, 2*a);
-    		qp2.set_c(Y, b);
-    		qp2.set_d(ZZ, ZZ, 2);
-
-    	// x + y >= -4
-    		qp2.set_a(X, 0, 1);
-    		qp2.set_a(Y, 0, 1);
-    		qp2.set_b(0, -4);
-       	// 4x + 2y  + zz >= -ab
-    		qp2.set_a(X, 1, 4);
-    		qp2.set_a(Y, 1, 2);
-    		qp2.set_a(ZZ, 1, 1);
-    		qp2.set_b(1, -a*b);
-
-    	// -x + y >= -1
-    		qp2.set_a(X, 2, -1);
-    		qp2.set_a(Y, 2, 1);
-    		qp2.set_b(2, -1);
-
-    	// X <= 0
-    		qp2.set_u(X, true, 0);
-    	// Y <= 0
-    		qp2.set_u(Y, true, 0);
-    	// ZZ >= 0
-    		qp2.set_l(ZZ, true, 0);
-
-
-		Solution s = (p==1 ?
-					  CGAL::solve_nonnegative_quadratic_program(qp1, ET()) :
-					  CGAL::solve_quadratic_program(qp2, ET()) );
-		if (s.is_infeasible()) std::cout << "no" << std::endl;
-		else if (s.is_unbounded()) std::cout << "unbounded" << std::endl;
-		else std::cout << (p==1?-1:1)*ceil_to_double(s.objective_value())
-			 << std::endl;
+    	Solution s = CGAL::solve_nonnegative_quadratic_program(qp, ET());
+    	if (s.is_optimal()) {
+			cout << floor(CGAL::to_double(s.objective_value())) << endl;
+		} else {
+			cout << "No such diet.\n";
+		}
 
 
     }
